2613e6bb1fb12e8732b79819df7cdd4b190f4e0a
Shaventus <maciej.ciosk@gmail.com>
2019-03-26 13:09:24 +0100

    added AccountService

--- CulinaryRecipes/src/main/java/com/culinaryrecipes/service/api/IAccountService.java
+++ CulinaryRecipes/src/main/java/com/culinaryrecipes/service/api/IAccountService.java
@@ -1,5 +1,22 @@
 package com.culinaryrecipes.service.api;
 
+import java.util.List;
+
+import com.culinaryrecipes.model.Account;
+import com.culinaryrecipes.model.dto.AccountDto;
+
 public interface IAccountService {
+	
+	void deleteById(int id); 
+	
+	void deleteByAccount(Account account);
+	
+	Account createAccount(AccountDto accountDto);
+	
+	Account getOne(int id);
+	
+	List<Account> findAll();
+	
+	List<Account> findByUsername(String username);
 
 }
