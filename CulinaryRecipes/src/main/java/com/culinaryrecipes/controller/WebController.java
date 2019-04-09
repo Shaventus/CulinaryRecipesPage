158479c57b91b13e7f79e8fe2e7e5534c94668c3
Shaventus <maciej.ciosk@gmail.com>
2019-04-09 22:53:25 +0200

    Ajax

--- CulinaryRecipes/src/main/java/com/culinaryrecipes/controller/WebController.java
+++ CulinaryRecipes/src/main/java/com/culinaryrecipes/controller/WebController.java
@@ -1,8 +1,11 @@
 package com.culinaryrecipes.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
@@ -12,15 +15,22 @@ public class WebController {
 		return "pages/public/index";
 	}
 	
+	
 	@GetMapping("/Test")
 	public String testpaget() {
 		return "Test";
 	}
+	
 	
 	@GetMapping("/index")
 	public String index() {
 
 	    return "pages/public/index";
 	}
+	
+	@RequestMapping(value = "/AjaxTest", produces = { MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.GET)
+    public @ResponseBody String pobierzProdukty(){             
+        return "Test";
+    }
 
 }
