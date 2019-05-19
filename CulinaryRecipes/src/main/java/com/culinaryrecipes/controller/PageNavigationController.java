d37aff82cc39b59a62f29a3ccb8af32628f93009
YunaAnn <ann.grzywnowicz@gmail.com>
2019-05-19 15:54:50 +0200

    Separate pages for signing in (signin.html) and signing up (signup.html, deleted signinsignup.html). Added pages: terms.html and privacy.html for all roles.

--- CulinaryRecipes/src/main/java/com/culinaryrecipes/controller/PageNavigationController.java
+++ CulinaryRecipes/src/main/java/com/culinaryrecipes/controller/PageNavigationController.java
@@ -14,17 +14,80 @@ public class PageNavigationController
 	{
 		return "pages/public/index";
 	}
-
 	
-	@RequestMapping(value = "/signinsignup", method = RequestMethod.GET)
-    public String login(Model model, String error, String logout) {
+	@GetMapping("/public/terms")
+	public String terms() 
+	{
+		return "pages/public/terms";
+	}
+
+	@GetMapping("/public/privacy")
+	public String privacy() 
+	{
+		return "pages/public/privacy";
+	}
+	
+	@RequestMapping(value = "/public/signin", method = RequestMethod.GET)
+    public String signin(Model model, String error, String logout) 
+	{
         if (error != null)
             model.addAttribute("loginError", "Your username and password are invalid.");
 
         if (logout != null)
             model.addAttribute("msg", "You have been logged out successfully.");
         
-        return "pages/public/signinsignup";
+        return "pages/public/signin";
     }
+	
+	@RequestMapping(value = "/public/signup", method = RequestMethod.GET)
+	public String signup() 
+	{
+		return "pages/public/signup";
+	}
+	
+	/**
+	 * admin
+	 * @return
+	 */
+	@GetMapping("/admin/home")
+	public String homeAdmin() 
+	{
+		return "pages/admin/index";
+	}
+	
+	@GetMapping("/admin/terms")
+	public String termsAdmin() 
+	{
+		return "pages/admin/terms";
+	}
+
+	@GetMapping("/admin/privacy")
+	public String privacyAdmin() 
+	{
+		return "pages/admin/privacy";
+	}
+	
+	/**
+	 * user
+	 * @return
+	 */
+	
+	@GetMapping("/user/home")
+	public String homeUser() 
+	{
+		return "pages/user/index";
+	}
+	
+	@GetMapping("/user/terms")
+	public String termsUser() 
+	{
+		return "pages/user/terms";
+	}
+
+	@GetMapping("/user/privacy")
+	public String privacyUser() 
+	{
+		return "pages/user/privacy";
+	}
 	
 }
